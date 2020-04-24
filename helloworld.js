d9144da5bc9ddeb099bbbef34b62251bbe31a8c9
Narra Jyothi <jyonarra@in.ibm.com>
2020-04-24 16:33:18 +0530

    sample node helloworld app

---
+++ helloworld.js
@@ -0,0 +1,49 @@
+var express = require('express')
+var app = express()
+
+
+const request = require('request');
+
+
+exports.getMessage = function (req, res) {
+
+	res.send("hello world from API");
+}
+
+exports.getGitApi = function (req, res) {
+
+	var response = {};
+	var projectarray = [];
+	getGitApiOutput(function (err, result) {
+		console.log("result --"+JSON.parse(result.body).length)
+	   projects = JSON.parse(result.body)
+	   projects.forEach(element => {
+		   if(element.site_admin == true)
+			projectarray.push(element);
+		});
+		res.send(projectarray);
+
+    });
+}
+
+function getGitApiOutput(cb) {
+	var options = {
+		url: "https://api.github.com/users?since=135",
+		method: 'GET',
+		headers: {
+			"User-Agent": "narra"
+		}
+	}
+    responseBody = {};
+	request(options, function (err,body) {
+
+		if (err){
+			console.log("errror is " + err);
+			cb(err,{})
+		}
+		else {
+
+			cb({},body);
+		}
+	})
+}
---
+++ index.js
@@ -0,0 +1,18 @@
+var express = require('express')
+var app = express()
+
+app.set('port', (process.env.PORT || 5000))
+app.use(express.static(__dirname + '/public'))
+
+var helloworld = require('./helloworld')
+app.get('/', function(request, response) {
+  response.send('Hello World!')
+})
+
+app.get('/getmsg',helloworld.getMessage);
+
+app.get('/getgitapi',helloworld.getGitApi);
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
